6c98f0d4e1e455610f490270dfb86e2e24f30eee
Malek <malek.alsibai@dynamics-regensburg.de>
2024-04-21 23:29:40 +0200

    Hello world

---
+++ calebYT/9_variables_expressions_statements.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int x; // variable declaration
+    x = 10; // variable initialization
+
+    int y = x/2;  //expression - anything that can be evaluated (to 1 value)
+    printf("%i", y);
+    return 0;
+}
