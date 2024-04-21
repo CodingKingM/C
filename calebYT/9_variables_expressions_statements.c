b76e0e8761a245dfd7a1c09d75339de73b51572d
Malek <malek.alsibai@dynamics-regensburg.de>
2024-04-21 23:31:32 +0200

    variables and expressions

--- calebYT/9_variables_expressions_statements.c
+++ calebYT/9_variables_expressions_statements.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main ()
+int main()
 {
 
-    int x; // variable declaration
+    int x;  // variable declaration
     x = 10; // variable initialization
 
-    int y = x/2;  //expression - anything that can be evaluated (to 1 value)
-    printf("%i", y);
+    int y = x / 2; // expression - anything that can be evaluated (to 1 value)
+    printf("The magic number of x is %i and y is %i", x, y);
     return 0;
 }
