d4573181124c7831bc6f8c28a8f69d9158bc75d6
Malek <malek.alsibai@dynamics-regensburg.de>
2024-04-27 18:38:07 +0200

    accepts CAPS input

--- cs50/agree.c
+++ cs50/agree.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
-int main (){
-
-char c;
-
-
-printf("Do you agree y/n?");
-scanf("%c", &c);
-
-if ( c == 'y')
+int main()
 {
-printf("Agreed\n");
-}
-else
-{
-    printf("Not agreed\n");
-}
+
+    char consent;
+
+    printf("Do you agree y/n?");
+    scanf("%c", &consent);
+
+    if (consent == 'y')
+    {
+        printf("Agreed\n");
+    }
+    else if (consent == 'Y')
+    {
+        printf("Agreed\n");
+    }
+    else if (consent == 'N')
+    {
+        printf("Not agreed");
+    }
+    else
+    {
+        printf("Not agreed\n");
+    }
     return 0;
 }
