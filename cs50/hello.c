1457079c8938fc252f9beae3bbbd1d025460a61f
Malek <malek.alsibai@dynamics-regensburg.de>
2024-04-27 17:07:04 +0200

    asking for input without cs50 lib

--- cs50/hello.c
+++ cs50/hello.c
@@ -2,6 +2,13 @@
 
 int main (){
 
-printf("hello world!\n");
+char firstName[10];
+char lastName[11];
+printf("What is your First name?\n");
+scanf("%s", &firstName);
+printf("What is your last name?\n");
+scanf("%s", &lastName);
+printf("Hello %s %s!", firstName, lastName);
+
     return 0;
 }
