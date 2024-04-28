e7059dea9e4cbdec6460e68adf2ecb14db68d852
Malek <malek.alsibai@dynamics-regensburg.de>
2024-04-28 22:33:55 +0200

    working

--- cs50/Lecture1/calculator.c
+++ cs50/Lecture1/calculator.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 int add(int a, int b);
-int main(){
+int main()
+{
+    int x;
+    int y;
+    printf("What is the value of x?\n");
+    scanf("%i", &x);
+    printf("What is the value of y?\n");
+    scanf("%i", &y);
+    
 
-int x;
-int y;
-
-
-printf("What is the value of x?\n");
-scanf("%i", &x);
-printf("What is the value of y?\n");
-scanf("%i", &y);
-
-int z = add(x, y);
-printf("= %i", z);
+    // int z = add(x, y);
+    printf("= %i", add(x, y));
     return 0;
 }
 
 int add(int a, int b)
 {
-return a + b;
-
+    return a + b;
 }
