f8ea73d465157a5b22b93b8128cb2e737aed7d9c
Malek <malek.alsibai@dynamics-regensburg.de>
2024-05-04 18:42:31 +0200

    CalculatorV2

---
+++ cs50/Lecture1/calculatorV2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    float x;
+    float y;
+    printf("What is the value of x?\n");
+    scanf("%i", &x);
+    printf("What is the value of y?\n");
+    scanf("%i", &y);
+    
+
+    // int z = add(x, y);
+    printf("= %f", x / y);
+    return 0;
+}
+
+
+ 
