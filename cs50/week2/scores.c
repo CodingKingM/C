7f4eb7a6aa4292a521f7b8951ba8510badf8f929
Malek <malek.alsibai@dynamics-regensburg.de>
2024-05-05 18:35:10 +0200

    array

--- cs50/week2/scores.c
+++ cs50/week2/scores.c
@@ -3,8 +3,12 @@
 int main()
 {
 
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+
+    printf("Average %f", (scores[0] + scores[1] + scores[2]) / 3.0);
     return 0;
 }
