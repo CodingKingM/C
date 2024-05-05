2448a21dd969324f21f5ff441affdb669d7a2f93
Malek <malek.alsibai@dynamics-regensburg.de>
2024-05-05 18:38:40 +0200

    dynamic but bad design

--- cs50/week2/scores.c
+++ cs50/week2/scores.c
@@ -5,9 +5,16 @@ int main()
 
     int scores[3];
 
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+printf("score:");
+scores[0];
+scanf("%i", &scores[0]);
+printf("score:");
+scores[1];
+scanf("%i", &scores[1]);
+printf("score:");
+scores[2];
+scanf("%i", &scores[2]);
+    
 
     printf("Average %f", (scores[0] + scores[1] + scores[2]) / 3.0);
     return 0;
