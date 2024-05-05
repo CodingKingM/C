7c8f3e82d0528834520f5b158a41b925da0d7ee9
Malek <malek.alsibai@dynamics-regensburg.de>
2024-05-05 17:40:30 +0200

    using debugger

--- cs50/week2/buggy.c
+++ cs50/week2/buggy.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
+void print_column(int height);
+
 int main()
 {
 
+    printf("Height:");
+    int h;
+    scanf("%i", &h);
+    print_column(h); 
+
+    return 0;
+}
+
+void print_column(int height)
+{
     for (int i = 0; i < 3; i++)
     {
-
-        //printf("i is %i\n", i); // can be used to debung/ to understand how the programm is running
         printf("#\n");
     }
-    return 0;
 }
