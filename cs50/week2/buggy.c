6cf6d82d9ede7c3f0236b09c07c3117306261f24
Malek <malek.alsibai@dynamics-regensburg.de>
2024-05-05 17:00:44 +0200

    printf to debug

--- cs50/week2/buggy.c
+++ cs50/week2/buggy.c
@@ -6,7 +6,7 @@ int main()
     for (int i = 0; i < 3; i++)
     {
 
-        printf("i is %i\n", i);
+        //printf("i is %i\n", i); // can be used to debung/ to understand how the programm is running
         printf("#\n");
     }
     return 0;
