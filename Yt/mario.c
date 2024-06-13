933f52f69cffe3ec777d4706da37b07b5e48471d
Malek <malek.alsibai03@gmail.com>
2024-06-13 16:15:02 +0200

    user input for nested loops

---
+++ Yt/mario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){
+
+int x;
+int y;
+
+printf("Rows: ");
+scanf("%i", &x);
+
+printf("Columns: ");
+scanf("%i", &y);
+
+for (int i = 0; i < x; i++)    // outer loop (rows)
+{
+    for (int j = 0; j<y; j++) // inner loop (column)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
+    return 0;
+}
